167449614501d635bd99169c1a7be517c214b41e
Chetan3327 <chauhanchetan12789@gmail.com>
2024-02-26 19:48:54 +0530

    frontend done

---
+++ Readme.md
@@ -0,0 +1,56 @@
+<!-- ABOUT THE PROJECT -->
+# Image Classification (CIFAR 10)
+![alt text](image-1.png)
+
+This **CIFAR-10 Image Classification App** is a web-based tool for predicting the class of images of the CIFAR-10 dataset.
+Users can upload an image, and the app employs a machine learning model to make predictions. The app is built with React for the frontend and FastAPI with Python for the backend. This app is specifically designed for classifying images into various categories within the CIFAR-10 dataset.
+
+
+
+Use the `README.md` to get started.
+
+<!-- BUILT WITH -->
+# Built With
+
+The Tech Stacks used are:
+
+<div align="center">
+  <a href="https://skillicons.dev">
+      <img src="https://skillicons.dev/icons?i=react,fastapi" />
+  </a>
+</div>
+
+## Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Chetan3327/image-classification.git
+   ```
+2. Navigate to the frontend directory:
+
+   ```bash
+   cd frontend
+   ```
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+4. Navigate to the backend directory:
+
+   ```bash
+   cd backend
+   ```
+5. Install dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+<!-- CONTACT -->
+# Contact
+
+Name: Chetan Chauhan - chauhanchetan12789@gmail.com
+
+Project Link: https://movie-recommendation-app-3327.netlify.app/
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState, DragEvent } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+type Prediction = {
+  class: string, 
+  confidence: number
+}
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const App = () => {
+  const [image, setImage] = useState<File | null>(null);
+  const [dragging, setDragging] = useState<boolean>(false)
+  const [selectedImage, setSelectedImage] = useState<string | null | undefined>(null)
+  const [prediction, setPrediction] = useState<Prediction | null>(null)
+  const [errorMessage, setErrorMessage] = useState<string>("")
+  
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrediction(null)
+    setImage(e.target.files[0])
+
+    const file = e.target.files[0]
+    if(file){
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setSelectedImage(e.target.result)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    setPrediction(null)
+    setImage(e.dataTransfer.files[0])
+
+    e.preventDefault();
+    setDragging(false);
+
+    const file = e.dataTransfer.files[0]
+    if(file){
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setSelectedImage(e.target.result)
+      }
+      reader.readAsDataURL(file)
+    }
+
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDragging(true)
+  }
+
+  const handleDragLeave = () => {
+    setDragging(false)
+  }
+
+  const handleSubmit = async () => {
+    if(!image){
+      setErrorMessage("Please upload an image to predict.")
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('file', image)
+
+    try {
+      const response = await axios.post(`${BACKEND_URL}/predict`, formData);
+
+      if (response.status === 200) {
+        const result = response.data;
+        setPrediction(result);
+        setErrorMessage('');
+      } else {
+        setErrorMessage('Error: Unable to make a prediction.');
+        setPrediction(null);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+      setErrorMessage('An error occurred while making the request.');
+      setPrediction(null);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex min-h-screen justify-center items-center bg-[#f0f0f0]">
+      <div className="bg-white rounded-xl shadow-lg p-5 text-center w-[90%] md:w-[400px]">
+        <h1 className="text-2xl font-bold mb-5 text-[#ff6b6b]">CIFAR 10</h1>  
+
+        <div onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop} className={`border-[2px] border-dashed cursor-pointer transition-colors duration-300 p-5 ${dragging && 'bg-[#f5f5f5]'}`}>
+          <p>Drag and drop an image here</p>
+          <label className="cursor-pointer text-[#007bff] underline" htmlFor="">
+            <input onChange={handleImageUpload} className="hidden" accept="image/*" type="file" />
+          </label>
+        </div>
+
+        {selectedImage && (<div className="flex flex-col items-center">
+          <h3 className="mt-5 text-gray-500">Upladed Image</h3>
+          <img className="w-[150px] h-auto" src={selectedImage} alt="selectedImage" />
+          <button onClick={handleSubmit} className="px-4 p-2 bg-[#007bff] text-white border-none rounded-md cursor-pointer mt-4 transition-colors duration-300 hover:bg-[#0056b3]">Predict</button>
+        </div>)}
+        
+        {prediction && (
+          <>
+            <p className="font-bold text-[#17a2b8] text-xl">{prediction.class}</p>
+            <p className="text-xl text-[#ff5733]">{Math.round(prediction.confidence * 100) / 100}%</p>
+          </>
+        )}
+
+        {errorMessage && (<div className="text-[#e74c3c] text-xs">{errorMessage}</div>)}
+      </div>      
+    </div>
   )
 }
 
